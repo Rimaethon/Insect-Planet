502b659e3506efbc6416de236ccedcecbe5c7211
Yoldaş Ulaş <yutalas@gmail.com>
2023-06-21 02:41:20 +0300

    Changes On Object Pool script
    It is more organized now some field names are changed

--- Assets/Insect_Planet/_Scripts/Enemies/EnemySpawner.cs
+++ Assets/Insect_Planet/_Scripts/Enemies/EnemySpawner.cs
@@ -1,57 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-/// <summary>
-/// This class handles spawning enemies
-/// </summary>
+
 public class EnemySpawner : MonoBehaviour
 {
-    [Tooltip("The prefab to use to spawn an enemy")]
-    public GameObject prefab = null;
+    public GameObject prefab;
 
-    /// <summary>
-    /// Enum to help with different kinds of spawners
-    /// </summary>
+    
     public enum SpawnMethod { Fixed, Random, Controlled }
 
-    [Tooltip("The intervals at which enemies are spawned:\n" +
-        "\tFixed: Enemies will spawn every n seconds where n is the spawn rate.\n" +
-        "\tRandom: Enemies will spawn at semi-random intervals, at most equal to the spawn rate.\n" +
-        "\tControlled: Enemies will spawn from this spawner only when something else calles the Spawn method")]
+ 
     public SpawnMethod spawnMethod = SpawnMethod.Fixed;
-    [Tooltip("The maximum time between spawns")]
     public float spawnRate = 5.0f;
-    [Tooltip("The size in each dimension of the area in which this spawner will spawn enemies.")]
     public Vector3 spawnAreaSize = Vector3.zero;
-    [Tooltip("Whether or not to display the spawn area for this spawner")]
     public bool showSpawnArea = true;
-    // The time at which the next enemy will be spawned
     private float nextSpawnTime = Mathf.NegativeInfinity;
 
-    /// <summary>
-    /// Description:
-    /// Standard Unity function called one per frame
-    /// Input: 
-    /// none
-    /// Return: 
-    /// void (no return)
-    /// </summary>
+  
     private void Update()
     {
-        // Every frame, try to spawn
         TestSpawn();
     }
 
-    /// <summary>
-    /// Description:
-    /// Standard Unity function that draws gizmos in the editor
-    /// Draws a red box where the spawn area is
-    /// Input: 
-    /// none
-    /// Return: 
-    /// void (no return)
-    /// </summary>
+   
     private void OnDrawGizmos()
     {
         if (showSpawnArea)
@@ -63,14 +33,7 @@ public class EnemySpawner : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// Description:
-    /// Tests whether it is time to spawn an enemy
-    /// Input: 
-    /// none
-    /// Return: 
-    /// void (no return)
-    /// </summary>
+  
     private void TestSpawn()
     {
         if (Time.timeSinceLevelLoad > nextSpawnTime)
@@ -79,14 +42,7 @@ public class EnemySpawner : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// Description:
-    /// Spawns an enemy if the prefab exists, also updates the next spawn time
-    /// Input: 
-    /// none
-    /// Return: 
-    /// void (no return)
-    /// </summary>
+   
     public void Spawn()
     {
         if (prefab != null)
@@ -104,19 +60,11 @@ public class EnemySpawner : MonoBehaviour
                     break;
             }
             Vector3 spawnLocation = GetSpawnLocation();
-            GameObject instance = GameObject.Instantiate(prefab, spawnLocation, Quaternion.identity, null);
+            GameObject instance = Instantiate(prefab, spawnLocation, Quaternion.identity, null);
         }
     }
 
-    /// <summary>
-    /// Description:
-    /// Determines the location at which to spawn an enemy
-    /// Input: 
-    /// none
-    /// Return: 
-    /// Vector3
-    /// </summary>
-    /// <returns>Vector3: The location at which to spawn the enemy prefab</returns>
+   
     public Vector3 GetSpawnLocation()
     {
         Vector3 result = Vector3.zero;
--- Assets/Insect_Planet/_Scripts/ObjectManagers/ObjectPool.cs
+++ Assets/Insect_Planet/_Scripts/ObjectManagers/ObjectPool.cs
@@ -5,25 +5,64 @@ namespace Insect_Planet._Scripts.ObjectManagers
 {
 	public sealed class ObjectPool : MonoBehaviour
 	{
-		public enum StartupPoolMode { Awake, Start, CallManually };
+		
 
+		#region Fields
+
+		public enum StartupPoolMode { Awake, Start, CallManually };
+		
 		[System.Serializable]
 		public class StartupPool
 		{
 			public int size;
 			public GameObject prefab;
 		}
+		static readonly List<GameObject> TempList = new List<GameObject>();
 
-		static ObjectPool _instance;
-		static List<GameObject> tempList = new List<GameObject>();
-	
-		Dictionary<GameObject, List<GameObject>> pooledObjects = new Dictionary<GameObject, List<GameObject>>();
-		Dictionary<GameObject, GameObject> spawnedObjects = new Dictionary<GameObject, GameObject>();
+		readonly Dictionary<GameObject, List<GameObject>> _pooledObjects = new Dictionary<GameObject, List<GameObject>>();
+		readonly Dictionary<GameObject, GameObject> _spawnedObjects = new Dictionary<GameObject, GameObject>();
 	
 		public StartupPoolMode startupPoolMode;
 		public StartupPool[] startupPools;
 
-		bool startupPoolsCreated;
+		bool _startupPoolsCreated;
+
+		#endregion
+		
+		
+		#region Singleton
+		
+		static ObjectPool _instance;
+
+		private static ObjectPool Instance
+		{
+			get
+			{
+				if (_instance != null)
+					return _instance;
+
+				_instance = Object.FindObjectOfType<ObjectPool>();
+				if (_instance != null)
+					return _instance;
+
+				var obj = new GameObject("ObjectPool")
+				{
+					transform =
+					{
+						localPosition = Vector3.zero,
+						localRotation = Quaternion.identity,
+						localScale = Vector3.one
+					}
+				};
+				_instance = obj.AddComponent<ObjectPool>();
+				return _instance;
+			}
+		}
+		#endregion
+
+		
+		
+		#region Unity Functions
 
 		void Awake()
 		{
@@ -38,16 +77,20 @@ namespace Insect_Planet._Scripts.ObjectManagers
 				CreateStartupPools();
 		}
 
+		#endregion
+		
+		
+		
+		#region Pool Creation
+
 		private static void CreateStartupPools()
 		{
-			if (!instance.startupPoolsCreated)
-			{
-				instance.startupPoolsCreated = true;
-				var pools = instance.startupPools;
-				if (pools != null && pools.Length > 0)
-					for (int i = 0; i < pools.Length; ++i)
-						CreatePool(pools[i].prefab, pools[i].size);
-			}
+			if (Instance._startupPoolsCreated) return;
+			Instance._startupPoolsCreated = true;
+			var pools = Instance.startupPools;
+			if (pools == null || pools.Length <= 0) return;
+			foreach (var t in pools)
+				CreatePool(t.prefab, t.size);
 		}
 
 		public static void CreatePool<T>(T prefab, int initialPoolSize) where T : Component
@@ -56,208 +99,31 @@ namespace Insect_Planet._Scripts.ObjectManagers
 		}
 		public static void CreatePool(GameObject prefab, int initialPoolSize)
 		{
-			if (prefab != null && !instance.pooledObjects.ContainsKey(prefab))
+			var active = prefab.activeSelf;
+			var parent = Instance.transform;
+			if (prefab == null || Instance._pooledObjects.ContainsKey(prefab)) return;
+			var list = new List<GameObject>();
+			Instance._pooledObjects.Add(prefab, list);
+
+			if (initialPoolSize <= 0) return;
+			prefab.SetActive(false);
+			while (list.Count < initialPoolSize)
 			{
-				var list = new List<GameObject>();
-				instance.pooledObjects.Add(prefab, list);
-
-				if (initialPoolSize > 0)
-				{
-					bool active = prefab.activeSelf;
-					prefab.SetActive(false);
-					Transform parent = instance.transform;
-					while (list.Count < initialPoolSize)
-					{
-						var obj = (GameObject)Object.Instantiate(prefab);
-						obj.transform.parent = parent;
-						list.Add(obj);
-					}
-					prefab.SetActive(active);
-				}
+				var obj = Instantiate(prefab, parent, true);
+				list.Add(obj);
 			}
-		}
-	
-		public static T Spawn<T>(T prefab, Transform parent, Vector3 position, Quaternion rotation) where T : Component
-		{
-			return Spawn(prefab.gameObject, parent, position, rotation).GetComponent<T>();
+			prefab.SetActive(active);
 		}
 
-		public static GameObject Spawn(GameObject prefab, Transform parent, Vector3 position, Quaternion rotation)
-		{
-			List<GameObject> list;
-			Transform trans;
-			GameObject obj;
-			if (instance.pooledObjects.TryGetValue(prefab, out list))
-			{
-				obj = null;
-				if (list.Count > 0)
-				{
-					while (obj == null && list.Count > 0)
-					{
-						obj = list[0];
-						list.RemoveAt(0);
-					}
-					if (obj != null)
-					{
-						trans = obj.transform;
-						trans.parent = parent;
-						trans.localPosition = position;
-						trans.localRotation = rotation;
-						obj.SetActive(true);
-						instance.spawnedObjects.Add(obj, prefab);
-						return obj;
-					}
-				}
-				obj = (GameObject)Object.Instantiate(prefab);
-				trans = obj.transform;
-				trans.parent = parent;
-				trans.localPosition = position;
-				trans.localRotation = rotation;
-				instance.spawnedObjects.Add(obj, prefab);
-				return obj;
-			}
-			else
-			{
-				obj = (GameObject)Object.Instantiate(prefab);
-				trans = obj.GetComponent<Transform>();
-				trans.parent = parent;
-				trans.localPosition = position;
-				trans.localRotation = rotation;
-				return obj;
-			}
-		}
-	
-		public static void ReturnToPool<T>(T obj) where T : Component
-		{
-			ReturnToPool(obj.gameObject);
-		}
-		public static void ReturnToPool(GameObject obj)
-		{
-			GameObject prefab;
-			if (instance.spawnedObjects.TryGetValue(obj, out prefab))
-				ReturnToPool(obj, prefab);
-			else
-				Object.Destroy(obj);
-		}
-		static void ReturnToPool(GameObject obj, GameObject prefab)
-		{
-			instance.pooledObjects[prefab].Add(obj);
-			instance.spawnedObjects.Remove(obj);
-			obj.transform.parent = instance.transform;
-			obj.SetActive(false);
-		}
-
-		public static void ReturnAllToPool<T>(T prefab) where T : Component
-		{
-			ReturnAllToPool(prefab.gameObject);
-		}
-		public static void ReturnAllToPool(GameObject prefab)
-		{
-			foreach (var item in instance.spawnedObjects)
-				if (item.Value == prefab)
-					tempList.Add(item.Key);
-			for (int i = 0; i < tempList.Count; ++i)
-				ReturnToPool(tempList[i]);
-			tempList.Clear();
-		}
-		public static void ReturnAllToPool()
-		{
-			tempList.AddRange(instance.spawnedObjects.Keys);
-			for (int i = 0; i < tempList.Count; ++i)
-				ReturnToPool(tempList[i]);
-			tempList.Clear();
-		}
-	
-		public static bool IsSpawned(GameObject obj)
-		{
-			return instance.spawnedObjects.ContainsKey(obj);
-		}
-
-		public static int CountPooled<T>(T prefab) where T : Component
-		{
-			return CountPooled(prefab.gameObject);
-		}
-		public static int CountPooled(GameObject prefab)
-		{
-			List<GameObject> list;
-			if (instance.pooledObjects.TryGetValue(prefab, out list))
-				return list.Count;
-			return 0;
-		}
-
-		public static int CountSpawned<T>(T prefab) where T : Component
-		{
-			return CountSpawned(prefab.gameObject);
-		}
-		public static int CountSpawned(GameObject prefab)
-		{
-			int count = 0 ;
-			foreach (var instancePrefab in instance.spawnedObjects.Values)
-				if (prefab == instancePrefab)
-					++count;
-			return count;
-		}
-
-		public static int CountAllPooled()
-		{
-			int count = 0;
-			foreach (var list in instance.pooledObjects.Values)
-				count += list.Count;
-			return count;
-		}
-
-		public static List<GameObject> GetPooled(GameObject prefab, List<GameObject> list, bool appendList)
-		{
-			if (list == null)
-				list = new List<GameObject>();
-			if (!appendList)
-				list.Clear();
-			List<GameObject> pooled;
-			if (instance.pooledObjects.TryGetValue(prefab, out pooled))
-				list.AddRange(pooled);
-			return list;
-		}
-		public static List<T> GetPooled<T>(T prefab, List<T> list, bool appendList) where T : Component
-		{
-			if (list == null)
-				list = new List<T>();
-			if (!appendList)
-				list.Clear();
-			List<GameObject> pooled;
-			if (instance.pooledObjects.TryGetValue(prefab.gameObject, out pooled))
-				for (int i = 0; i < pooled.Count; ++i)
-					list.Add(pooled[i].GetComponent<T>());
-			return list;
-		}
-
-		public static List<GameObject> GetSpawned(GameObject prefab, List<GameObject> list, bool appendList)
-		{
-			if (list == null)
-				list = new List<GameObject>();
-			if (!appendList)
-				list.Clear();
-			foreach (var item in instance.spawnedObjects)
-				if (item.Value == prefab)
-					list.Add(item.Key);
-			return list;
-		}
-		public static List<T> GetSpawned<T>(T prefab, List<T> list, bool appendList) where T : Component
-		{
-			if (list == null)
-				list = new List<T>();
-			if (!appendList)
-				list.Clear();
-			var prefabObj = prefab.gameObject;
-			foreach (var item in instance.spawnedObjects)
-				if (item.Value == prefabObj)
-					list.Add(item.Key.GetComponent<T>());
-			return list;
-		}
-
+		#endregion
+		
+		
+		
+		#region Pool Destruction
 		public static void DestroyPooled(GameObject prefab)
 		{
 			List<GameObject> pooled;
-			if (instance.pooledObjects.TryGetValue(prefab, out pooled))
+			if (Instance._pooledObjects.TryGetValue(prefab, out pooled))
 			{
 				for (int i = 0; i < pooled.Count; ++i)
 					GameObject.Destroy(pooled[i]);
@@ -278,25 +144,208 @@ namespace Insect_Planet._Scripts.ObjectManagers
 		{
 			DestroyAll(prefab.gameObject);
 		}
-
-		public static ObjectPool instance
+		
+		
+		
+		#endregion
+		
+		
+		
+		#region Spawning and Returning
+		
+			public static T Spawn<T>(T prefab, Transform parent, Vector3 position, Quaternion rotation) where T : Component
 		{
-			get
-			{
-				if (_instance != null)
-					return _instance;
-
-				_instance = Object.FindObjectOfType<ObjectPool>();
-				if (_instance != null)
-					return _instance;
-
-				var obj = new GameObject("ObjectPool");
-				obj.transform.localPosition = Vector3.zero;
-				obj.transform.localRotation = Quaternion.identity;
-				obj.transform.localScale = Vector3.one;
-				_instance = obj.AddComponent<ObjectPool>();
-				return _instance;
-			}
+			return Spawn(prefab.gameObject, parent, position, rotation).GetComponent<T>();
 		}
+
+		public static GameObject Spawn(GameObject prefab, Transform parent, Vector3 position, Quaternion rotation)
+		{
+			List<GameObject> list;
+			Transform trans;
+			GameObject obj;
+			if (Instance._pooledObjects.TryGetValue(prefab, out list))
+			{
+				obj = null;
+				if (list.Count > 0)
+				{
+					while (obj == null && list.Count > 0)
+					{
+						obj = list[0];
+						list.RemoveAt(0);
+					}
+					if (obj != null)
+					{
+						trans = obj.transform;
+						trans.parent = parent;
+						trans.localPosition = position;
+						trans.localRotation = rotation;
+						obj.SetActive(true);
+						Instance._spawnedObjects.Add(obj, prefab);
+						return obj;
+					}
+				}
+				obj = Instantiate(prefab);
+				trans = obj.transform;
+				trans.parent = parent;
+				trans.localPosition = position;
+				trans.localRotation = rotation;
+				Instance._spawnedObjects.Add(obj, prefab);
+				return obj;
+			}
+
+			obj = Instantiate(prefab);
+			trans = obj.GetComponent<Transform>();
+			trans.parent = parent;
+			trans.localPosition = position;
+			trans.localRotation = rotation;
+			return obj;
+		}
+	
+		public static void ReturnToPool<T>(T obj) where T : Component
+		{
+			ReturnToPool(obj.gameObject);
+		}
+		public static void ReturnToPool(GameObject obj)
+		{
+			GameObject prefab;
+			if (Instance._spawnedObjects.TryGetValue(obj, out prefab))
+				ReturnToPool(obj, prefab);
+			else
+				Object.Destroy(obj);
+		}
+		static void ReturnToPool(GameObject obj, GameObject prefab)
+		{
+			Instance._pooledObjects[prefab].Add(obj);
+			Instance._spawnedObjects.Remove(obj);
+			obj.transform.parent = Instance.transform;
+			obj.SetActive(false);
+		}
+
+		public static void ReturnAllToPool<T>(T prefab) where T : Component
+		{
+			ReturnAllToPool(prefab.gameObject);
+		}
+		public static void ReturnAllToPool(GameObject prefab)
+		{
+			foreach (var item in Instance._spawnedObjects)
+				if (item.Value == prefab)
+					TempList.Add(item.Key);
+			for (int i = 0; i < TempList.Count; ++i)
+				ReturnToPool(TempList[i]);
+			TempList.Clear();
+		}
+		public static void ReturnAllToPool()
+		{
+			TempList.AddRange(Instance._spawnedObjects.Keys);
+			for (int i = 0; i < TempList.Count; ++i)
+				ReturnToPool(TempList[i]);
+			TempList.Clear();
+		}
+	
+		#endregion
+
+		
+		
+		#region Utility Functions
+
+		public static bool IsSpawned(GameObject obj)
+		{
+			return Instance._spawnedObjects.ContainsKey(obj);
+		}
+
+		public static int CountPooled<T>(T prefab) where T : Component
+		{
+			return CountPooled(prefab.gameObject);
+		}
+		public static int CountPooled(GameObject prefab)
+		{
+			List<GameObject> list;
+			if (Instance._pooledObjects.TryGetValue(prefab, out list))
+				return list.Count;
+			return 0;
+		}
+
+		public static int CountSpawned<T>(T prefab) where T : Component
+		{
+			return CountSpawned(prefab.gameObject);
+		}
+		public static int CountSpawned(GameObject prefab)
+		{
+			int count = 0 ;
+			foreach (var instancePrefab in Instance._spawnedObjects.Values)
+				if (prefab == instancePrefab)
+					++count;
+			return count;
+		}
+
+		public static int CountAllPooled()
+		{
+			int count = 0;
+			foreach (var list in Instance._pooledObjects.Values)
+				count += list.Count;
+			return count;
+		}
+
+		#endregion
+
+		
+		
+		#region Get Objects
+
+		public static List<GameObject> GetPooled(GameObject prefab, List<GameObject> list, bool appendList)
+		{
+			if (list == null)
+				list = new List<GameObject>();
+			if (!appendList)
+				list.Clear();
+			List<GameObject> pooled;
+			if (Instance._pooledObjects.TryGetValue(prefab, out pooled))
+				list.AddRange(pooled);
+			return list;
+		}
+		public static List<T> GetPooled<T>(T prefab, List<T> list, bool appendList) where T : Component
+		{
+			if (list == null)
+				list = new List<T>();
+			if (!appendList)
+				list.Clear();
+			List<GameObject> pooled;
+			if (Instance._pooledObjects.TryGetValue(prefab.gameObject, out pooled))
+				for (int i = 0; i < pooled.Count; ++i)
+					list.Add(pooled[i].GetComponent<T>());
+			return list;
+		}
+
+		public static List<GameObject> GetSpawned(GameObject prefab, List<GameObject> list, bool appendList)
+		{
+			if (list == null)
+				list = new List<GameObject>();
+			if (!appendList)
+				list.Clear();
+			foreach (var item in Instance._spawnedObjects)
+				if (item.Value == prefab)
+					list.Add(item.Key);
+			return list;
+		}
+		public static List<T> GetSpawned<T>(T prefab, List<T> list, bool appendList) where T : Component
+		{
+			if (list == null)
+				list = new List<T>();
+			if (!appendList)
+				list.Clear();
+			var prefabObj = prefab.gameObject;
+			foreach (var item in Instance._spawnedObjects)
+				if (item.Value == prefabObj)
+					list.Add(item.Key.GetComponent<T>());
+			return list;
+		}
+
+
+		#endregion
+
+	
+		
+
+		
 	}
 }
