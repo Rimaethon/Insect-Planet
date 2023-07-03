f058b607f6600b25dad5879c61c7d172ccf3ef68
Yoldaş Ulaş <yutalas@gmail.com>
2023-07-03 20:05:51 +0300

    Some changes on UI
    UI element positions are changed

--- Assets/Rimaethon/_Scripts/UI/LoadingSceneController.cs
+++ Assets/Rimaethon/_Scripts/UI/LoadingSceneController.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Rimaethon._Scripts.Utility;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class LoadingSceneController : MonoBehaviour
 {
     private CanvasGroup loadingScreen;
@@ -21,40 +23,50 @@ public class LoadingSceneController : MonoBehaviour
     private void Awake()
     {
         loadingScreen = GetComponent<CanvasGroup>();
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        
+        Debug.Log(loadingScreen.alpha+"alpha is this and ");
+
     }
 
     private void LoadScene(int sceneID)
     {
-        StartCoroutine(LoadSceneAsync(sceneID));
+        gameObject.SetActive(true);
+        StartCoroutine(FadeCanvasGroup(loadingScreen, 1, fadeDuration));
     }
 
     IEnumerator LoadSceneAsync(int sceneID)
     {
+        
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneID);
-
+        
         while (!operation.isDone)
         {
-            float progress = Mathf.Clamp01(operation.progress / 0.9f);
-            yield return FadeCanvasGroup(loadingScreen, progress, fadeDuration);
+            yield return null;
         }
 
-        yield return FadeCanvasGroup(loadingScreen, 0f, fadeDuration); // Unfade the loading screen
+        yield return StartCoroutine(FadeCanvasGroup(loadingScreen, 0, fadeDuration));
     }
 
     IEnumerator FadeCanvasGroup(CanvasGroup canvasGroup, float targetAlpha, float duration)
     {
         float startAlpha = canvasGroup.alpha;
         float time = 0.0f;
-        float actualDuration = Mathf.Max(duration, 0.5f); // Ensure a minimum duration of 0.5 seconds
+        float actualDuration = Mathf.Max(duration, 0.5f); // Ensure a minimum duration of 0.5 second
 
         while (time < actualDuration)
         {
-            float alpha = Mathf.Lerp(startAlpha, targetAlpha, time / actualDuration);
+            float alpha = Mathf.Lerp(startAlpha, targetAlpha,actualDuration );
             canvasGroup.alpha = alpha;
             yield return null;
             time += Time.deltaTime;
         }
 
         canvasGroup.alpha = targetAlpha;
+        yield return StartCoroutine(LoadSceneAsync(1));
     }
 }
