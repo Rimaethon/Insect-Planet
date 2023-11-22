6660123e61b31d2022e035689ff2cc07525d1027
Yoldaş Ulaş <yutalas@gmail.com>
2023-11-22 14:48:05 +0300

    CameraController's Process Rotation method is replaced with Look method
    Instead of using ifs for clamping the rotation, it is much more better to just use clamp(clever right?) . Also look input is now coming from OnPlayerLook event

--- Assets/Rimaethon/_Scripts/Controller/CameraController.cs
+++ Assets/Rimaethon/_Scripts/Controller/CameraController.cs
@@ -1,111 +1,53 @@
-﻿using UnityEngine;
+﻿using Rimaethon._Scripts.Utility;
+using Rimaethon.Scripts.Managers;
+using UnityEngine;
 
-/// <summary>
-///     This class uses processed input from the input manager to control the vertical rotation of the camera
-/// </summary>
+
 public class CameraController : MonoBehaviour
 {
-    [Header("Settings")] [Tooltip("The camera to have control over")]
-    public Camera controledCamera;
+    [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private float lookSpeed = 1f; 
+    [SerializeField] private int downClamp = 30;
+    [SerializeField] private int upClamp = -30;
+    private Vector3 _moveVector; 
+    private Vector2 _lookVector; 
 
-    [Tooltip("The speed at which the camera rotates to look up and down (calculated in degrees)")]
-    public float rotationSpeed = 60f;
-
-    [Tooltip("Whether or not to invert the look direction")]
-    public bool invert = true;
-
-    private int framesWaited;
-
-    // The input manager to read input from
-    private InputManager inputManager;
-
-    // Wait this many frames before starting to process the camera rotation
-    private readonly int waitForFrames = 3;
-
-    /// <summary>
-    ///     Description:
-    ///     Standard Unity function called once before the first Update call
-    ///     Input:
-    ///     none
-    ///     Return:
-    ///     void (no return)
-    /// </summary>
-    private void Start()
+    private void Awake()
     {
-        SetUpCamera();
-        SetUpInputManager();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    /// <summary>
-    ///     Description:
-    ///     Standard Unity function called once every frame
-    ///     Input:
-    ///     none
-    ///     Return:
-    ///     void (no return)
-    /// </summary>
+    private void OnEnable()
+    {
+        EventManager.Instance.AddHandler<Vector2>(GameEvents.OnPlayerLook, HandlePlayerLook);
+    }
+
+    private void OnDisable()
+    {
+        if (EventManager.Instance == null) return;
+        EventManager.Instance.RemoveHandler<Vector2>(GameEvents.OnPlayerLook, HandlePlayerLook);
+    }
+
+    private void HandlePlayerLook(Vector2 lookVector)
+    {
+        _lookVector = lookVector;
+    }
+    private float Xrotation;
+
+    private void Look()
+    {
+        float horizontalLookInput = _lookVector.x * lookSpeed * Time.deltaTime;
+        float verticalLookInput = _lookVector.y * lookSpeed * Time.deltaTime;
+        Vector3 newRotation = transform.eulerAngles;
+        Xrotation -= verticalLookInput;
+        Xrotation = Mathf.Clamp(Xrotation, downClamp, upClamp);
+        newRotation.x = Xrotation;
+        newRotation.y += horizontalLookInput;
+        transform.eulerAngles = newRotation;
+    
+    }
     private void Update()
     {
-        // Wait so many frames to avoid startup camera movement bug
-        if (framesWaited <= waitForFrames)
-        {
-            framesWaited += 1;
-            return;
-        }
-
-        ProcessRotation();
-    }
-
-    /// <summary>
-    ///     Description:
-    ///     Sets up the camera component if not already donw
-    ///     Input:
-    ///     none
-    ///     Return:
-    ///     void (no return)
-    /// </summary>
-    private void SetUpCamera()
-    {
-        if (controledCamera == null) controledCamera = GetComponent<Camera>();
-    }
-
-    /// <summary>
-    ///     Description:
-    ///     Gets the input manager from the scene
-    ///     Input:
-    ///     none
-    ///     Return:
-    ///     void (no return)
-    /// </summary>
-    private void SetUpInputManager()
-    {
-        inputManager = FindObjectOfType<InputManager>();
-    }
-
-    /// <summary>
-    ///     Description:
-    ///     Process the vertical look input to rotate the player accordingly
-    ///     Input:
-    ///     none
-    ///     Return:
-    ///     void (no return)
-    /// </summary>
-    private void ProcessRotation()
-    {
-        var verticalLookInput = inputManager.verticalLookAxis;
-        var cameraRotation = controledCamera.transform.rotation.eulerAngles;
-        float newXRotation = 0;
-        if (invert)
-            newXRotation = cameraRotation.x - verticalLookInput * rotationSpeed * Time.deltaTime;
-        else
-            newXRotation = cameraRotation.x + verticalLookInput * rotationSpeed * Time.deltaTime;
-
-        // clamp the rotation 360 - 270 is up 0 - 90 is down
-        // Because of the way eular angles work with Unity's rotations we have to act differently when clamping the rotation
-        if (newXRotation < 270 && newXRotation >= 180)
-            newXRotation = 270;
-        else if (newXRotation > 90 && newXRotation < 180) newXRotation = 90;
-        controledCamera.transform.rotation =
-            Quaternion.Euler(new Vector3(newXRotation, cameraRotation.y, cameraRotation.z));
+        Look();
     }
 }
