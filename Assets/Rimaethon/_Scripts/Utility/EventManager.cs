0124f6f71eef80d2b5adaf7bf4411c6f4140e1fd
Yoldaş Ulaş <yutalas@gmail.com>
2023-12-09 11:50:10 +0300

    EventManager is Updated and IEventManager is deleted
    EventManager is converted to Persistent Singleton since it needs to handle the events in scene change.  Also now it handles PlayerInput events specifically.

--- Assets/Rimaethon/_Scripts/Utility/EventManager.cs
+++ Assets/Rimaethon/_Scripts/Utility/EventManager.cs
@@ -1,190 +1,132 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Rimaethon._Scripts.Utility;
+using Rimaethon.Scripts.Utility;
+using Rimaethon.Utility;
 using UnityEngine;
 
-namespace Rimaethon._Scripts.Utility
+namespace Rimaethon.Scripts.Managers
 {
-    public class EventManager
+    public class EventManager : PersistentSingleton<EventManager>
     {
-        #region Event Priorities
+        #region Fields And Properties
 
-        public void SetEventPriority(GameEvents gameEvents, int priority)
-        {
-            _eventPriorities[gameEvents] = priority;
-        }
-
-        #endregion
-
-        #region Fields
-
-        private readonly Dictionary<GameEvents, List<Delegate>> _eventHandlers = new();
-        private readonly Dictionary<GameEvents, int> _eventPriorities = new();
-        private readonly Queue<Action> _eventQueue = new();
-        private bool _isProcessingEvent;
+        //public SerializableDictionary<GameEvents, List<Delegate>> _eventHandlers = new SerializableDictionary<GameEvents, List<Delegate>> ();
+        public Dictionary<GameEvents, List<Delegate>> _eventHandlers = new();
+        [SerializeField] protected LogSystem logSystem;
 
         #endregion
 
 
-        #region Singleton
+        #region Unity Methods
 
-        private static EventManager _instance;
-
-        public static EventManager Instance
+        protected override void Awake()
         {
-            get { return _instance ??= new EventManager(); }
+            if (logSystem != null)
+            {
+                logSystem.sender = this;
+            }
+            base.Awake();
+        }
+ 
+        [SerializeField] private List<string> _eventNames = new();
+
+        private void Start()
+        {
+            foreach (var events in _eventHandlers.Keys)
+            {
+                _eventNames.Add(events.ToString());
+                foreach (var value in _eventHandlers[events].ToList())
+                {
+                    Type type = value.Method.DeclaringType;
+                    string className = type.Name;
+                    _eventNames.Add(className+ "." + value.Method.Name);
+                }
+               
+            }
         }
 
-        private EventManager()
+        protected override void OnApplicationQuit()
         {
+            _eventHandlers.Clear();
+            Debug.LogWarning("Event Manager is cleared");
+            base.OnApplicationQuit();
         }
 
         #endregion
 
         #region Event Handlers
 
-        public void AddHandler(GameEvents gameEvent, Action handler)
+        public void AddHandler(GameEvents playerInput, Action handler)
         {
-            if (!_eventHandlers.ContainsKey(gameEvent)) _eventHandlers[gameEvent] = new List<Delegate>();
+            if (!_eventHandlers.ContainsKey(playerInput)) _eventHandlers[playerInput] = new List<Delegate>();
 
-            _eventHandlers[gameEvent].Add(handler);
-            Debug.Log($"Added handler {handler.Method.Name} for game event {gameEvent}");
-
-
-            Debug.Log($"Subscribed handler {handler.Method.Name} to game event {gameEvent}");
+            _eventHandlers[playerInput].Add(handler);
+            logSystem.PromptLog($"Added handler {handler.Method.Name} for game event {playerInput}");
         }
 
-        public void AddHandler<T>(GameEvents gameEvent, Action<T> handler)
+        public void AddHandler<T>(GameEvents playerInput, Action<T> handler)
         {
-            if (!_eventHandlers.ContainsKey(gameEvent))
-                _eventHandlers[gameEvent] = new List<Delegate>();
+            if (!_eventHandlers.ContainsKey(playerInput)) _eventHandlers[playerInput] = new List<Delegate>();
 
-            _eventHandlers[gameEvent].Add(handler);
-            Debug.Log($"Added handler {handler.Method.Name} for game event {gameEvent}");
+            _eventHandlers[playerInput].Add(handler);
+            logSystem.PromptLog($"Added handler {handler.Method.Name} for game event {playerInput}");
         }
 
-        public void AddHandler<T1, T2>(GameEvents gameEvent, Action<T1, T2> handler)
+        public void RemoveHandler(GameEvents playerInput, Action handler)
         {
-            if (!_eventHandlers.ContainsKey(gameEvent))
-                _eventHandlers[gameEvent] = new List<Delegate>();
-
-            _eventHandlers[gameEvent].Add(handler);
-            Debug.Log($"Added handler {handler.Method.Name} for game event {gameEvent}");
-        }
-
-
-        public void RemoveHandler(GameEvents gameEvent, Action handler)
-        {
-            if (_eventHandlers.TryGetValue(gameEvent, out var handlers))
+            if (_eventHandlers.TryGetValue(playerInput, out var handlers))
             {
                 handlers.Remove(handler);
-                Debug.Log($"Removed handler {handler.Method.Name} for game event {gameEvent}");
+                logSystem.PromptLog($"Removed handler {handler.Method.Name} for game event {playerInput}");
 
                 if (handlers.Count == 0)
                 {
-                    _eventHandlers.Remove(gameEvent);
-                    Debug.Log($"No more handlers for game event {gameEvent}");
+                    _eventHandlers.Remove(playerInput);
+                    logSystem.PromptLog($"No more handlers for game event {playerInput}");
                 }
             }
         }
 
-        public void RemoveHandler<T>(GameEvents gameEvent, Action<T> handler)
+        public void RemoveHandler<T>(GameEvents playerInput, Action<T> handler)
         {
-            if (_eventHandlers.TryGetValue(gameEvent, out var handlers))
+            if (_eventHandlers.TryGetValue(playerInput, out var handlers))
             {
                 handlers.Remove(handler);
-                Debug.Log($"Removed handler {handler.Method.Name} for game event {gameEvent}");
+                logSystem.PromptLog($"Removed handler {handler.Method.Name} for game event {playerInput}");
 
                 if (handlers.Count == 0)
                 {
-                    _eventHandlers.Remove(gameEvent);
-                    Debug.Log($"No more handlers for game event {gameEvent}");
+                    _eventHandlers.Remove(playerInput);
+                    logSystem.PromptLog($"No more handlers for game event {playerInput}");
                 }
             }
         }
-
-        public void RemoveHandler<T1, T2>(GameEvents gameEvent, Action<T1, T2> handler)
-        {
-            if (_eventHandlers.TryGetValue(gameEvent, out var handlers))
-            {
-                handlers.Remove(handler);
-                Debug.Log($"Removed handler {handler.Method.Name} for game event {gameEvent}");
-
-                if (handlers.Count == 0)
-                {
-                    _eventHandlers.Remove(gameEvent);
-                    Debug.Log($"No more handlers for game event {gameEvent}");
-                }
-            }
-        }
-
-        // RemoveHandler overloads for different parameter types
 
         #endregion
 
-
         #region Event Broadcasting
 
-        public void Broadcast(GameEvents gameEvents)
+        public void Broadcast(GameEvents playerInputs)
         {
-            _eventQueue.Enqueue(() => ProcessEvent(gameEvents));
-            ProcessEventQueueWithPriority();
+            ProcessEvent(playerInputs);
         }
 
-        public void Broadcast<T>(GameEvents gameEvent, T arg)
+        public void Broadcast<T>(GameEvents playerInput, T arg)
         {
-            _eventQueue.Enqueue(() => ProcessEvent(gameEvent, arg));
-            ProcessEventQueueWithPriority();
+            ProcessEvent(playerInput, arg);
         }
 
-        public void Broadcast<T1, T2>(GameEvents gameEvent, T1 arg1, T2 arg2)
+        private void ProcessEvent(GameEvents playerInputs, params object[] args)
         {
-            _eventQueue.Enqueue(() => ProcessEvent(gameEvent, arg1, arg2));
-            ProcessEventQueueWithPriority();
-        }
-
-
-        private void ProcessEventQueueWithPriority()
-        {
-            if (_isProcessingEvent) return;
-
-            var sortedEvents = new List<Action>(_eventQueue);
-            sortedEvents.Sort((a, b) => GetEventPriority(b) - GetEventPriority(a));
-
-            // Process events in the sorted queue
-            while (sortedEvents.Count > 0)
-            {
-                var eventAction = sortedEvents[0];
-                sortedEvents.RemoveAt(0);
-                eventAction.DynamicInvoke();
-            }
-        }
-
-        private int GetEventPriority(Action eventAction)
-        {
-            foreach (var eventHandlers in _eventHandlers)
-                if (eventHandlers.Value.Contains(eventAction))
-                {
-                    if (_eventPriorities.TryGetValue(eventHandlers.Key, out var priority)) return priority;
-                    break;
-                }
-
-            return 0;
-        }
-
-        private void ProcessEvent(GameEvents gameEvents, params object[] args)
-        {
-            _isProcessingEvent = true;
-
-            if (_eventHandlers.TryGetValue(gameEvents, out var eventHandler))
+            if (_eventHandlers.TryGetValue(playerInputs, out var eventHandler))
                 foreach (var handler in eventHandler)
                 {
                     handler.DynamicInvoke(args);
-                    Debug.Log(
-                        $"Broadcasted event {gameEvents.ToString()} with arguments {string.Join(", ", args.Select(arg => arg.ToString()))} to handler {handler.Method.Name}");
+                    logSystem.PromptLog(
+                        $"Broadcasted event {playerInputs} with arguments {string.Join(", ", args.Select(arg => arg.ToString()))} to handler {handler.Method.Name}");
                 }
-
-            _isProcessingEvent = false;
         }
 
         #endregion
