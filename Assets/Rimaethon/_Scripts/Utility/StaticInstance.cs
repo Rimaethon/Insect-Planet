df1b0cd934d180159814908925f553ad7397b515
Yoldaş Ulaş <yutalas@gmail.com>
2024-02-19 16:54:23 +0300

    Static Instance Class is extended with Private and Private Persistent abstract classes
    I needed some Static Instances that are not accessible from outside but can check if there is another instance of them. Classes Such as Scene Controller can only be accessed with events but there needs to be only one instance and that instance needs to be persistent. So I created Private Singleton and Private Persistent Singleton abstract classes so that classes that needs this type of functionality can inherit them.

--- Assets/Rimaethon/_Scripts/Utility/StaticInstance.cs
+++ Assets/Rimaethon/_Scripts/Utility/StaticInstance.cs
@@ -1,34 +1,127 @@
+using System;
 using UnityEngine;
 
-namespace Rimaethon._Scripts.Utility
+namespace Rimaethon.Scripts.Utility
 {
+    #region Static Instance
+
     public abstract class StaticInstance<T> : MonoBehaviour where T : MonoBehaviour
     {
-        public static T Instance { get; private set; }
+        private static T _instance;
+
+        private static bool IsApplicationQuitting { get; set; }
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance != null) return _instance;
+
+                _instance = FindObjectOfType<T>();
+
+
+                if (_instance == null && !IsApplicationQuitting)
+                    Debug.LogError($"Instance of type {typeof(T)} could not be found.");
+                Debug.Log($"Instance of type {typeof(T)} found.");
+
+                return _instance;
+            }
+            protected set => _instance = value;
+        }
+
 
         protected virtual void Awake()
         {
-            Instance = this as T;
+            InitializeInstance();
         }
 
         protected virtual void OnApplicationQuit()
         {
-            Instance = null;
-            Destroy(gameObject);
+            IsApplicationQuitting = true;
+        }
+
+        private void InitializeInstance()
+        {
+            if (this is T instance)
+            {
+                Instance = instance;
+                Debug.Log($"Instance of type {typeof(T)} created.");
+            }
+            else
+            {
+                Debug.LogError($"Instance of type {typeof(T)} could not be created.");
+                throw new InvalidOperationException($"Instance of type {typeof(T)} could not be created.");
+            }
         }
     }
 
+    #endregion
+
+
+    #region Singleton
 
     public abstract class Singleton<T> : StaticInstance<T> where T : MonoBehaviour
     {
         protected override void Awake()
         {
-            if (Instance != null) Destroy(gameObject);
-            base.Awake();
+            if (this is T instance)
+            {
+                if (Instance != null && Instance != this)
+                    Destroy(gameObject);
+                else
+                    Instance = instance;
+            }
+            else
+            {
+                Debug.LogError($"Instance of type {typeof(T)} could not be created.");
+                throw new InvalidOperationException($"Instance of type {typeof(T)} could not be created.");
+            }
         }
     }
 
+    #endregion
+
+
+    #region DontDestroyOnLoad
+
     public abstract class PersistentSingleton<T> : Singleton<T> where T : MonoBehaviour
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    #endregion
+
+
+    #region Private Singleton
+
+    public abstract class PrivateSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T _instance;
+
+        protected virtual void Awake()
+        {
+            if (this is T instance)
+            {
+                if (_instance != null && _instance != this)
+                    Destroy(gameObject);
+                else
+                    _instance = instance;
+            }
+            else
+            {
+                Debug.LogError($"Instance of type {typeof(T)} could not be created.");
+                throw new InvalidOperationException($"Instance of type {typeof(T)} could not be created.");
+            }
+        }
+    }
+
+    #endregion
+
+    public abstract class PrivatePersistentSingleton<T> : PrivateSingleton<T> where T : MonoBehaviour
     {
         protected override void Awake()
         {
