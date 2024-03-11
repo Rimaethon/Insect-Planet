3c9458aeb561037512c2da2faa79d275d5c3360d
Yoldaş Ulaş <yutalas@gmail.com>
2024-03-11 15:09:15 +0300

    UI Panel Opener Button is created

---
+++ Assets/Rimaethon/_Scripts/UI/UIPanelOpenerButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Rimaethon._Scripts.UI
+{
+	public class UIPanelOpenerButton: UIButton
+	{
+		[SerializeField] private RectTransform _panelToOpen;
+		[SerializeField] private RectTransform _panelToClose;
+		private void OpenPanel()
+		{
+			StartCoroutine(UIAnimationHelperMethods.SlideIn(_panelToOpen, UIDirection.UP, 2f));
+			StartCoroutine(UIAnimationHelperMethods.SlideOut(_panelToClose, UIDirection.UP, 2f));
+		}
+		protected override void DoOnClicked()
+		{
+			OpenPanel();
+		}
+	}
+}
